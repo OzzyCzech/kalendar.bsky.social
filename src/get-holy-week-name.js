f9a074f62d32a6b55dbdc38fddd3cb8025adf8ca
Roman Ožana <roman@ozana.cz>
2025-04-13 11:10:42 +0200

    ✝

--- src/get-holy-week-name.js
+++ src/get-holy-week-name.js
@@ -4,7 +4,7 @@ export function getHolyWeekName(date) {
 	let meta = getDayMeta(date.toJSDate());
 	// Easter days in Holy Week
 	if (meta.isHolyWeek && !meta.easter.isEasterMonday && !meta.easter.isGoodFriday) {
-		return ` († ${meta.easter.name})`;
+		return ` (✝ ${meta.easter.name})`;
 	}
 
 	return "";
